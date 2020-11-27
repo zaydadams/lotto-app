c8cef9747b765393bcfce593c5db514bac3200e8
zaydadams <zaydadams6@gmail.com>
2020-11-27 10:24:46 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import random
+from random import sample
 from tkinter import messagebox
 # Login GUI
 window = Tk()
@@ -18,14 +18,15 @@ lbl_2 = Label(window, text='Age ')
 lbl_1.place(x=10, y=55)
 lbl_2.place(x=30, y=125)
 
+
 # functions for the first GUI
 def login():
     age = int(age_ent.get())
     ages = (age)
-    if ages <18:
+    if ages < 18:
         messagebox.showerror('Error', 'You must be over 18')
         window.withdraw()
-    elif ages >=18:
+    elif ages >= 18:
         messagebox.showinfo('Welcome Particapant', 'Are you ready to be a Millionare ' + full_nm.get())
         window.withdraw()
         logged_in()
@@ -39,9 +40,10 @@ def logged_in():
     root = Tk()
     root.geometry('700x400')
     root.title('Lotto Ticket Generator')
-
+    import random
     lbl1 = Label(root, text='Lotto Generator', font=["Arial", 20])
     lbl1.pack()
+
 
     ent1 = Entry(root, width='2')
     ent2 = Entry(root, width='2')
@@ -50,12 +52,51 @@ def logged_in():
     ent5 = Entry(root, width='2')
     ent6 = Entry(root, width='2')
 
-    ent1.place(x=5, y=10)
-    ent2.place(x=5, y=50)
+    ent1.place(x=180, y=80)
+    ent2.place(x=210, y=80)
+    ent3.place(x=240, y=80)
+    ent4.place(x=270, y=80)
+    ent5.place(x=300, y=80)
+    ent6.place(x=330, y=80)
+
+    def user():
+        num1=int(ent1.get())
+        num2=int(ent2.get())
+        num3=int(ent3.get())
+        num4=int(ent4.get())
+        num5=int(ent5.get())
+        num6=int(ent6.get())
+
+        list=[num1,num2,num3,num4,num5,num6]
+        return list
+
+    label1=Label(root,text="", bg='blue',fg='white')
+    label1.place(x = 100, y= 120)
+
+    def generate():
+        r = sample(range(1, 49),6)
+        r.sort()
+
+        label1.configure(text=r)
+
+        counter= 0
+
+        for number in user():
+            if number in r:
+                counter += 1
+            messagebox.showinfo("attention")
+
+        elif:
+
+            if number in r:
+                counter += 2
+            messagebox.showinfo("you only won a Rand")
+
+
+    btn = Button(root, text="generate", command=generate)
+    btn.place(x=120, y=150)
 
     root.mainloop()
 
 
 window.mainloop()
-
-
