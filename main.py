52b67421d4ad8f0c1c008c410205815ceb3fa33e
zaydadams <zaydadams6@gmail.com>
2020-11-30 13:47:08 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import *
 from random import sample
 from tkinter import messagebox
+from datetime import *
+
 # Login GUI
 window = Tk()
 window.geometry('350x350')
 window.title('Login Form')
+window.config(bg='grey')
 
 full_nm = Entry(window)
 age_ent = Entry(window)
@@ -12,8 +15,8 @@ age_ent = Entry(window)
 full_nm.place(x=90, y=50)
 age_ent.place(x=90, y=120)
 
-lbl_1 = Label(window, text='Full name ')
-lbl_2 = Label(window, text='Age ')
+lbl_1 = Label(window, text='Full name ', bg='black', fg='white')
+lbl_2 = Label(window, text='Age ', bg='black', fg='white')
 
 lbl_1.place(x=10, y=55)
 lbl_2.place(x=30, y=125)
@@ -32,51 +35,64 @@ def login():
         logged_in()
 
 
-btn1 = Button(window, text='Login', bg='green', width='17', height='2', command=login)
+btn1 = Button(window, text='Login', bg='black', fg='white', width='17', height='2', command=login)
 btn1.place(x=90, y=200)
 
-
+#second GUI
 def logged_in():
     root = Tk()
     root.geometry('700x400')
     root.title('Lotto Ticket Generator')
-    lbl1 = Label(root, text='Lotto Generator', font=["Arial", 20])
+    root.config(bg='grey')
+    lbl1 = Label(root, text='Lotto Generator', font=["Arial", 20], bg='black', fg='white')
     lbl1.pack()
 
+    ent1 = Entry(root, border='3', width='2')
+    ent2 = Entry(root, border='3', width='2')
+    ent3 = Entry(root, border='3', width='2')
+    ent4 = Entry(root, border='3', width='2')
+    ent5 = Entry(root, border='3', width='2')
+    ent6 = Entry(root, border='3', width='2')
 
-    ent1 = Entry(root, width='2')
-    ent2 = Entry(root, width='2')
-    ent3 = Entry(root, width='2')
-    ent4 = Entry(root, width='2')
-    ent5 = Entry(root, width='2')
-    ent6 = Entry(root, width='2')
-
-    ent1.place(x=180, y=80)
-    ent2.place(x=210, y=80)
-    ent3.place(x=240, y=80)
-    ent4.place(x=270, y=80)
-    ent5.place(x=300, y=80)
-    ent6.place(x=330, y=80)
+    ent1.place(x=350, y=120)
+    ent2.place(x=400, y=120)
+    ent3.place(x=450, y=120)
+    ent4.place(x=500, y=120)
+    ent5.place(x=550, y=120)
+    ent6.place(x=600, y=120)
 
     def user():
-        num1=int(ent1.get())
-        num2=int(ent2.get())
-        num3=int(ent3.get())
-        num4=int(ent4.get())
-        num5=int(ent5.get())
-        num6=int(ent6.get())
+        num1 = int(ent1.get())
+        num2 = int(ent2.get())
+        num3 = int(ent3.get())
+        num4 = int(ent4.get())
+        num5 = int(ent5.get())
+        num6 = int(ent6.get())
 
         list=[num1,num2,num3,num4,num5,num6]
         return list
 
-    label1=Label(root,text="", bg='blue',fg='white')
-    label1.place(x = 100, y= 120)
+    label1= Label(root, text="", bg='black', fg='white')
+    label1.place(x=500, y=200)
+
+    label2= Label(root, text="Winning Numbers: ", bg='black', fg='white')
+    label2.place(x=350, y=200)
+
 
     def generate():
         r = sample(range(1, 50), 6)
         r.sort()
 
         label1.configure(text=r)
+
+        a= open("lottery.txt","a+")
+        txt = "The lucky numbers are: " +str(r)
+        txt1 = "The Users entries: " +str(user())
+        today = datetime.now()
+        day = today.strftime("%d %B %Y")
+        time = today.strftime("%H:%M %p")
+        data = "Name: "+full_nm.get() +"\n"+txt+"\n"+txt1+"\nDate: "+str(day)+"\nTime: "+str(time)+"\n="+"\n"
+        a.write(data)
 
         counter = 0
 
@@ -85,26 +101,27 @@ def logged_in():
                 counter += 1
 
         if counter <= 1:
-            messagebox.showinfo("attention", "you a unlucky poes")
+            messagebox.showerror("", "unlucky try again")
 
-        elif counter <= 2:
-            messagebox.showinfo("attention", "you Won a R10 go buy you R10 mtn")
+        elif counter == 2:
+            messagebox.showerror("", "unlucky try again")
 
-        elif counter <= 3:
-            messagebox.showinfo("attention", "you Won Something")
+        elif counter == 3:
+            messagebox.showerror("", "Unlucky try again")
 
-        elif counter <= 4:
-            messagebox.showinfo("attention", "you Won Something")
+        elif counter == 4:
+            messagebox.showerror("", "Unlucky try again")
 
-        elif counter <= 5:
-            messagebox.showinfo("attention", "you Won Something")
+        elif counter == 5:
+            messagebox.showerror("", "Unlucky try again")
 
-        elif counter <= 6:
-            messagebox.showinfo("attention", "you Won Something")
+        elif counter == 6:
+            messagebox.showinfo("", "You Won")
 
 
-    btn = Button(root, text="generate", command=generate)
-    btn.place(x=120, y=150)
+
+    btn = Button(root, text="generate", bg='black', fg='white', command=generate)
+    btn.place(x=120, y=122)
 
     def clear():
         ent1.delete(0, END)
@@ -113,11 +130,20 @@ def logged_in():
         ent4.delete(0, END)
         ent5.delete(0, END)
         ent6.delete(0, END)
-        label1.delete(0, END)
 
+    def exit():
+        root.destroy()
 
-    btn2 =Button(root, text='clear', command=clear)
-    btn2.pack()
+    ext_btn = Button(root, text='exit', bg='black', fg='white', command=exit)
+    ext_btn.place(x=180, y=200)
+
+    btn2 =Button(root, text='clear', bg='black', fg='white', command=clear)
+    btn2.place(x=100, y=200)
+
+    def generate():
+        text = open("lottery.txt","a+")
+        text.write("Lotto numbers"+str(generate()))
+
     root.mainloop()
 
 
